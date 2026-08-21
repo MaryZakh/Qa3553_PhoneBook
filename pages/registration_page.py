2fd8d719c522f06f3a02159a6535abb3813ee81a
Mariia Zakharevich <z0559882272@gmail.com>
2026-08-21 09:26:43 +0300

    added registration class

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -4,19 +4,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-class LoginPage:
-    LOGIN_NAV_LINK = (By.CSS_SELECTOR, "[href='/login']")
+class RegistrationPage:
+    REGISTRATION_NAV_LINK = (By.CSS_SELECTOR, "[href='/login']")
     EMAIL_INPUT = (By.CSS_SELECTOR, "input[name='email']")
     PASSWORD_INPUT = (By.CSS_SELECTOR, "input[name='password']")
-    LOGIN_BTN = (By.XPATH, "//button[text()='Login']")
+    REGISTRATION_BTN = (By.XPATH, "//button[text()='Registration']")
     SIGN_OUT_BTN = (By.XPATH,"//*[text()='Sign Out']")
 
     def __init__(self, driver):
         self.driver = driver
 
 
-    def open_login_form(self):
-        self.driver.find_element(*self.LOGIN_NAV_LINK).click()
+    def open_registration_form(self):
+        self.driver.find_element(*self.REGISTRATION_NAV_LINK).click()
 
     def fill_email(self,email):
         self.driver.find_element(*self.EMAIL_INPUT).clear()
@@ -26,18 +26,10 @@ class LoginPage:
         self.driver.find_element(*self.PASSWORD_INPUT).clear()
         self.driver.find_element(*self.PASSWORD_INPUT).send_keys(password)
 
-    def submit_login(self):
-        self.driver.find_element(*self.LOGIN_BTN).click()
+    def submit_registration(self):
+        self.driver.find_element(*self.REGISTRATION_BTN).click()
 
-    # def is_logged(self):
-    #     try:
-    #         self.driver.find_element(*self.SIGN_OUT_BTN)
-    #         return True
-    #     except NoSuchElementException:
-    #         return False
-
-
-    def is_logged(self):
+    def is_registered(self):
         try:
             WebDriverWait(self.driver,timeout=5).until(
                 EC.visibility_of_element_located(self.SIGN_OUT_BTN)
