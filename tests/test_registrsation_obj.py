55cf5dcafc70671b2b43b947e1aae56c5bb002cf
Mariia Zakharevich <z0559882272@gmail.com>
2026-08-25 18:16:51 +0300

    added registration_tests with object

--- tests/test_registrsation_obj.py
+++ tests/test_registrsation_obj.py
@@ -1,17 +1,22 @@
-from pages.registration_page import RegistrationPage
+import uuid
 
-VALID_EMAIL = "margo_12346@gmail.com"
-VALID_PASSWORD = "Mmar123456$"
-INVALID_EMAIL = "margo123gmail.com"
-INVALID_PASSWORD = "Mmar123"
+from models.user import User
+from pages.registration_page import RegistrationPage
 
 
 def test_registration_success(driver):
     registration_page = RegistrationPage(driver)
 
+    random_suffix = uuid.uuid4().hex[:8]
+
+    user = User(
+        f"mariia_test_{random_suffix}@gmail.com",
+        "Password123$"
+    )
+
     registration_page.open_registration_form()
-    registration_page.fill_email(VALID_EMAIL)
-    registration_page.fill_password(VALID_PASSWORD)
+    registration_page.fill_email(user.username)
+    registration_page.fill_password(user.password)
     registration_page.submit_registration()
 
     assert registration_page.is_registered() is True
@@ -20,9 +25,14 @@ def test_registration_success(driver):
 def test_registration_wrong_email(driver):
     registration_page = RegistrationPage(driver)
 
+    user = User(
+        "mariia_testgmail.com",
+        "Password123$"
+    )
+
     registration_page.open_registration_form()
-    registration_page.fill_email(INVALID_EMAIL)
-    registration_page.fill_password(VALID_PASSWORD)
+    registration_page.fill_email(user.username)
+    registration_page.fill_password(user.password)
     registration_page.submit_registration()
 
     assert "Wrong email or password format" in registration_page.get_alert_text()
@@ -31,21 +41,32 @@ def test_registration_wrong_email(driver):
 def test_registration_wrong_password(driver):
     registration_page = RegistrationPage(driver)
 
+    user = User(
+        "mariia_test@gmail.com",
+        "Pa123$"
+    )
+
     registration_page.open_registration_form()
-    registration_page.fill_email(VALID_EMAIL)
-    registration_page.fill_password(INVALID_PASSWORD)
+    registration_page.fill_email(user.username)
+    registration_page.fill_password(user.password)
     registration_page.submit_registration()
 
     assert "Wrong email or password format" in registration_page.get_alert_text()
     registration_page.accept_alert()
 
+
 def test_registration_exists_user(driver):
     registration_page = RegistrationPage(driver)
 
+    user = User(
+        "margo@gmail.com",
+        "Mmar123456$"
+    )
+
     registration_page.open_registration_form()
-    registration_page.fill_email("margo@gmail.com")
-    registration_page.fill_password("Mmar123456$")
+    registration_page.fill_email(user.username)
+    registration_page.fill_password(user.password)
     registration_page.submit_registration()
 
-    assert registration_page.get_alert_text()=="User already exist"
+    assert registration_page.get_alert_text() == "User already exist"
     registration_page.accept_alert()
